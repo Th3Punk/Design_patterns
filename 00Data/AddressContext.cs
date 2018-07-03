69512f21cfeda3e138c54dab1c513cd89a9b132c
Sitkei Balázs <sitkei.balazs92@gmail.com>
2018-07-03 10:36:19 +0200

    #1: -Dataproject added       -EntityFramework reference added, but not work exactly

---
+++ 00Data/AddressContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace _00Data
+{
+    public class AddressContext : DbContext
+    {
+        public DbSet<MyAddress> MyAddress { get; set; }
+    }
+}
