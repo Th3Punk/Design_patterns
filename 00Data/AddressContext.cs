a9d63eb7d5a5f6c5cdb919debe267fcd2e5fb9ef
Sitkei Balázs <sitkei.balazs92@gmail.com>
2018-07-05 17:34:28 +0200

    #1: EF codefirst repaired and first migration steps added

--- 00Data/AddressContext.cs
+++ 00Data/AddressContext.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Data.Entity;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
 
 namespace _00Data
 {
     public class AddressContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=./punkserver; Database=test; Trusted_Connection=True;"
+                );
+        }
+
         public DbSet<MyAddress> MyAddress { get; set; }
     }
 }
---
+++ 00Data/Migrations/20180705153243_CreateDefaultDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _00Data.Migrations
+{
+    public partial class CreateDefaultDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MyAddress",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyAddress", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyAddress");
+        }
+    }
+}
